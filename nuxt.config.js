ab1ede0027ebfa15d56364e491c80a060c3deed1
Pooya Golchian <pooya.golchian@gmail.com>
2022-05-28 16:02:19 +0400

    update proxy for axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/api', '~/plugins/vue-toast'],
+  plugins: ['~/plugins/api', '~/plugins/vue-toast.client'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- plugins/vue-toast.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Toasted from 'vue-toasted'
-
-Vue.use(Toasted, {
-  duration: 2000,
-})
