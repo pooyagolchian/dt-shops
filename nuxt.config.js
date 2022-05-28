d894034ba244e0f3827de09ed82dcc94365d81c5
Pooya Golchian <pooya.golchian@gmail.com>
2022-05-28 15:55:56 +0400

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  target: 'static',
-  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - dt-shops-product',
