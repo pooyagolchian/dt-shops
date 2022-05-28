c88405acfd2806e2b2110a7b8d1c58b481038fbf
Pooya Golchian <pooya.golchian@gmail.com>
2022-05-28 15:29:43 +0400

    test deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,15 +51,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    proxy: true,
-  },
-
-  proxy: {
-    '/api/': {
-      target: 'https://dvs-api.dtone.com/v1',
-      pathRewrite: { '^/api/': '' },
-      changeOrigin: true,
-    },
+    proxy: false,
+    prefix: process.env.API_URL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
