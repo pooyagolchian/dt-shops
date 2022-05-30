43e5f6a036d91790cfe7ee671180200ea05ad9d9
Mohamad Hallal <mohamad.hallal@transfer-to.com>
2022-05-30 19:53:24 -0400

    Update Cypress interceptor

--- cypress/mock/api.js
+++ cypress/mock/api.js
@@ -2,7 +2,7 @@ export const mockedProductsEndpoint = () => {
   cy.intercept(
     {
       method: 'GET',
-      url: 'https://dvs-api.dtone.com/v1/products?page=1&per_page=100',
+      url: 'https://preprod-dvs-api.dtone.com/v1/products?page=1&per_page=100',
     },
     { fixture: '../fixtures/products.json' }
   )
